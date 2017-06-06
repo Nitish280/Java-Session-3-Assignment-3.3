5ba1de90f15a81cc0dc670fec51f44eb4b8e2386
Nitish280 <nsrccs@gmail.com>
2017-06-06 19:57:22 +0530

    truthtable

---
+++ src/TruthTable.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TruthTable {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in  = new Scanner(System.in);
+		
+		System.out.println("Input first number: ");  
+	    byte num1 = in.nextByte();
+	    		
+	    System.out.println("Input first number: ");
+	    byte num2 = in.nextByte();
+	    
+	    int a=0,b=1;
+	    System.out.println(a&b); 
+	    System.out.println(a==0 && b==0);
+	 
+	    }
+
+}
